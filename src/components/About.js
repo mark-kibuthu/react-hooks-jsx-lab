55e94b932850dec4456bfa15588d44491c0dab21
Agent-grey <markkibuthu@gmail.com>
2024-08-05 23:28:19 +0300

    complete lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,13 @@
-import React from "react";
-import { image } from "../data/data";
-
-function About() {
-  return <div>About</div>;
-}
+import React from 'react';
+import { image } from '../data/data'; 
+const About = () => {
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>This is some content about me.</p> 
+      <img src={image} alt="I made this" />
+    </div>
+  );
+};
 
 export default About;
